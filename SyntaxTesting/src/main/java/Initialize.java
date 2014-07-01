f758e3d53ddafd043cd2012836704a4a38d35f66
Katie Scott <kls82@cam.ac.uk>
2014-07-01 13:21:35 +0100

    put hi comment

--- SyntaxTesting/src/main/java/Initialize.java
+++ SyntaxTesting/src/main/java/Initialize.java
@@ -3,6 +3,7 @@ import com.puppycrawl.tools.checkstyle.Main;
 public class Initialize {
 
 	public static void main(String[] args) {
+		//hi
 		final String dir = System.getProperty("user.dir");
 		String[] comLineArgs = {"-c" + dir + "\\src\\main\\resources\\CheckstyleFormat.xml" , "-o" + dir + "\\src\\main\\resources\\CheckstyleOutput.txt ", dir + "\\src\\main\\java\\Test.java"};
 
