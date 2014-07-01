ca0735fa4d07ca1c8abcb01e173b7ce0b4c783d1
Katie Scott <kls82@cam.ac.uk>
2014-07-01 12:46:08 +0100

    should now work

--- SyntaxTesting/src/main/java/Initialize.java
+++ SyntaxTesting/src/main/java/Initialize.java
@@ -1,12 +1,12 @@
 import com.puppycrawl.tools.checkstyle.Main;
 
-public class Initialise {
+public class Initialize {
 
 	public static void main(String[] args) {
 		final String dir = System.getProperty("user.dir");
 		String[] comLineArgs = {"-c" + dir + "\\src\\main\\resources\\CheckstyleFormat.xml" , "-o" + dir + "\\src\\main\\resources\\CheckstyleOutput.txt ", dir + "\\src\\main\\java\\Test.java"};
 
-		Main.main(comLineArgs);
+		Initialize.main(comLineArgs);
 	}
 }
 
